d2d92fc0bf6f763b766a74833480f81970a85ef0
Tarek Mohammad <tarekmohammad022@gmail.com>
2026-08-18 22:06:31 +0300

    feat(core): setup network client, routing, and design system tokens

---
+++ lib/apps/core/router/routes.dart
@@ -0,0 +1,9 @@
+abstract final class Routes {
+  static const String onboarding = '/';
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String otpVerification = '/otp-verification';
+  static const String resetPassword = '/reset-password';
+  static const String roleSelection = '/role-selection';
+  static const String home = '/home';
+}
